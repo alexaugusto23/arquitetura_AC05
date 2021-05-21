29ee596e306cfcd888690df8551bd200ec65e57a
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-05-21 18:02:25 -0300

    21052021

--- docker_primos/app.py
+++ docker_primos/app.py
@@ -42,4 +42,4 @@ def verifica_primo(candidato_primo):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT",5000))
-    app.run(debug=True, host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
